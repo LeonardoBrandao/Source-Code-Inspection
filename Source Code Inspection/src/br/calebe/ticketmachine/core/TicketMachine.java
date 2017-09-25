4f92776b1db441d6ad8e3b547ae35779d426671d
Felipe Estrela <estrela.felipe.cardoso@hotmail.com>
2017-09-24 22:20:32 -0300

    Alteração na classe TicketMachine.java, para realizar validação da nota segundo a regra de exceção. close #3

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -3,6 +3,7 @@ package br.calebe.ticketmachine.core;
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
 import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  *
@@ -13,6 +14,7 @@ public class TicketMachine {
     protected int valor;
     protected int saldo;
     protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected Scanner in = new Scanner(System.in);
 
     public TicketMachine(int valor) {
         this.valor = valor;
@@ -22,22 +24,29 @@ public class TicketMachine {
     public void inserir(int quantia) {
         try {
             boolean achou = false;
+            
             for (int i = 0; i < papelMoeda.length && !achou; i++) {
-                if (papelMoeda[1] == quantia) {
+                if (papelMoeda[i] == quantia) {
                     achou = true;
                     System.out.println("Moeda aceita!");
                 }
             }
-            
+
             if (!achou) {
                 throw new PapelMoedaInvalidaException();
             }
             this.saldo += quantia;
-            
-        } catch (PapelMoedaInvalidaException e) {
-            System.err.println("Erro papel Moeda Invalida");
-        }
 
+        } catch (PapelMoedaInvalidaException e) {
+            System.out.println("Sua nota não foi aceita! Por favor retirar! "
+                    + "Digite 1 para informar que retirou sua nota!!");
+            int retirou = in.nextInt();
+            
+            while(retirou != 1){
+                System.out.println("Por favor retirar a nota!");
+                retirou = in.nextInt();
+            }             
+        }
     }
 
     public int getSaldo() {
